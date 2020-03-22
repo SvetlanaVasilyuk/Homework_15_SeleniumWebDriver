af2e9c3e9de470c05dddc3ec47b7288dd23724ed
SvetlanaVasilyuk <lanarusufa@gmail.com>
2020-03-23 02:18:17 +0300

    Добавлена задача 15 Работа с Selenium WebDriver (поиск в яндексе)

--- src/test/java/com/github/SvetlanaVasilyuk/YandexTest.java
+++ src/test/java/com/github/SvetlanaVasilyuk/YandexTest.java
@@ -4,32 +4,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 public class YandexTest {
 
     private WebDriver webDriver;
 
     @BeforeMethod
-    public void initDriver(){
+    public void initDriver() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         webDriver = new ChromeDriver();
     }
 
     @Test
-    public void testYandex(){
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    public void testYandex() throws InterruptedException {
         webDriver.get("https://yandex.ru");
+        WebElement input = webDriver.findElement(By.id("text"));
+        input.sendKeys("руддщ цщкдв");
+
+        // Поиск через xpath
+        WebElement button = webDriver.findElement(By.xpath("//child::div[@class=\"search2__button\"]"));
+
+        // Поиск элемента через родительский по классу, затем по вложенному в него тегу
+        //WebElement buttonDiv = webDriver.findElement(By.className("search2__button"));
+        //WebElement button = buttonDiv.findElement(By.tagName("button"));
+
+        button.click();
+        WebElement newInput = webDriver.findElement(By.name("text"));
+        Assert.assertEquals(newInput.getAttribute("value"), "hello world");
+        String tab = webDriver.getTitle();
+        Assert.assertTrue(tab.contains("hello world"));
     }
 
     @AfterMethod
-    public void closeBrowser(){
+    public void closeBrowser() {
         webDriver.quit();
     }
 
